0b01e04c8e5d44f8c2397b83ee1f734951abb2da
langcao365 <82774751+langcao365@users.noreply.github.com>
2024-10-23 23:02:37 -0400

    Add files via upload

---
+++ Lab_6_Version_Control_Lang_Cao.py
@@ -0,0 +1,34 @@
+def encode(passwd):
+    data = []
+    for c in passwd:
+        newc = str((int(c) + 3) % 10)
+        data.append(newc)
+    return "".join(newc)
+
+def decode(passwd):
+    pass
+    return 0 # this is a placeholder
+
+running = True
+encpass = 0
+while running:
+    print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+""")
+    option = input("Please enter an option:")
+    if option == "1":
+        p = input("Please enter your password to encode: ")
+        encpass = encode(p)
+        del p
+        print("Your password has been encoded and stored!")
+    elif option == "2":
+        print(f"The encoded password is {encpass}, and the original password is {decode(encpass)}")
+    elif option == "3":
+        running = False
+
+        
+    
+    
